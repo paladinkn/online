42d13bff6131445c262e180861ecc6e7056d1bc9
yangtian <2458897613@qq.comn>
2016-11-18 10:51:08 +0800

    st

--- src/js/public/basic/left.js
+++ src/js/public/basic/left.js
@@ -5,27 +5,36 @@ import React, { PropTypes, Component } from 'react';
 import { Link } from 'react-router';
 
 class Left extends Component {
+	leftContr() {
+		var url = window.location.href.split('#')[1].split('?')['0'].split('/')[1];
+		if(!url) {
+			url = 'home';
+		}
+		return url;
+	}
 	render() {
+		var _thisUrl = this.leftContr();
+
 		return (
 			<div className = 'left_con'>
 				<ul>
 					<Link to='/home'>
-						<li>
+						<li className = { _thisUrl == 'home'?'th_color':'' } >
 							home
 						</li>
 					</Link>
 					<Link to='/demo'>
-						<li>
+						<li className = { _thisUrl == 'demo'?'th_color':'' } >
 							demo
 						</li>
 					</Link>
 					<Link to='/online'>
-						<li>
+						<li className = { _thisUrl == 'online'?'th_color':'' } >
 							online
 						</li>
 					</Link>
 					<Link to='/table'>
-						<li>
+						<li className = { _thisUrl == 'table'?'th_color':'' } >
 							table
 						</li>
 					</Link>
